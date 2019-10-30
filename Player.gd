6f6150f073608deed845eb265004e3ae7cc919b5
BradleyKost <32468102+BradleyKost@users.noreply.github.com>
2019-10-29 23:23:53 -0500

    [US 139] Player FSM
    Added a simple player fsm with a few staes, added a double jump and changed how the character movement direction is detected.

--- Player.gd
+++ Player.gd
@@ -1,34 +1,33 @@
 extends KinematicBody2D
 
-const SPEED = 100
-const GRAVITY = 10
-const JUMP_POWER = -200
+const SPEED = 200
+const GRAVITY = 600
+const JUMP_POWER = -300
 const FLOOR = Vector2(0, -1)
+const MAX_JUMPS = 2
 
 var velocity = Vector2()
+var jumps = MAX_JUMPS
 var on_ground = false
 
-func _physics_process(delta):
-	if Input.is_action_pressed("ui_right"):
-		velocity.x = SPEED
-	elif Input.is_action_pressed("ui_left"):
-		velocity.x = -SPEED
-	else:
-		velocity.x = 0
-		
-	max(3,3);
+
+func _apply_gravity(delta):
+	velocity.y += GRAVITY * delta
 	
-	
-	# Jumping
-	if Input.is_action_pressed("ui_up") && on_ground == true:
-		velocity.y = JUMP_POWER
-		on_ground = false
-	
-	velocity.y += GRAVITY
-	
+func _apply_movement():
 	if is_on_floor():
 		on_ground = true
+		jumps = MAX_JUMPS
 	else:
 		on_ground = false
 	
 	velocity = move_and_slide(velocity, FLOOR)
+
+func _handle_move_input():
+	var move_direction = -int(Input.is_action_pressed("move_left")) + int(Input.is_action_pressed("move_right"))
+	velocity.x = lerp(velocity.x, SPEED * move_direction, 0.2)
+	if move_direction != 0:
+		$Sprite.scale.x = move_direction
+		
+func _is_on_ground():
+	return on_ground
---
+++ PlayerFSM.gd
@@ -0,0 +1,72 @@
+extends "res://StateMachine.gd"
+
+
+# handles the player input
+func _input(event):
+	if [states.idle, states.run, states.fall, states.jump].has(state):
+		if event.is_action_pressed("jump") && parent.jumps > 0:
+			parent.jumps -= 1
+			parent.velocity.y = parent.JUMP_POWER
+
+# initialized the states of the player
+# sets initial state to idle
+func _ready():
+	add_state("idle")
+	add_state("run")
+	add_state("jump")
+	add_state("fall")
+	call_deferred("set_state", states.idle)
+
+func _state_logic(delta):
+	parent._handle_move_input()
+	parent._apply_gravity(delta)
+	parent._apply_movement()
+	
+func _get_transition(delta):
+	# this determines the transitions of the state machine
+	# this is where all the logic for state transitions should happen
+	match state:
+		# conditions for transitions from idle state
+		states.idle:
+			if !parent._is_on_ground():
+				if parent.velocity.y < 0:
+					return states.jump
+				elif parent.velocity.y > 0:
+					return states.fall
+			elif parent.velocity.x > 0.01 && parent.velocity.x < -0.01:
+				return states.run
+		# conditions for transitions from run state
+		states.run:
+			if !parent._is_on_ground():
+				if parent.velocity.y < 0:
+					return states.jump
+				elif parent.velocity.y > 0:
+					return states.fall
+			elif parent.velocity.x < 0.01 && parent.velocity.x > -0.01:
+				return states.idle
+		states.jump:
+			if parent._is_on_ground():
+				return states.idle
+			elif parent.velocity.y >= 0:
+				return states.fall
+		states.fall:
+			if parent._is_on_ground():
+				return states.idle
+			elif parent.velocity.y < 0:
+				return states.jump
+				
+	return null
+	
+func _enter_state(new_state, old_state):
+	match new_state:
+		states.idle:
+			parent.get_node("State Label").set_text("idle")
+		states.run:
+			parent.get_node("State Label").set_text("run")
+		states.jump:
+			parent.get_node("State Label").set_text("jump")
+		states.fall:
+			parent.get_node("State Label").set_text("fall")
+	
+func _exit_state(old_state, new_state):
+	pass

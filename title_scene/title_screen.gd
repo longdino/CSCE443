b631abebfdb41a60f7d773c629fe0d6535d37bb4
hayoungjeon <hayoungjeon@tamu.edu>
2019-11-06 13:29:14 -0600

    [US 150] Exit button added

--- title_scene/title_screen.gd
+++ title_scene/title_screen.gd
@@ -6,8 +6,14 @@ var scene_path_to_load
 func _ready():
 	$Menu/CenterRow/Buttons/NewGameButton.grab_focus()
 	for button in $Menu/CenterRow/Buttons.get_children():
-		button.connect("pressed", self, "_on_Button_pressed", [button.scene_to_load])
+		if button.name == "ExitButton":
+			button.connect("pressed", self, "quit")
+		else:
+			button.connect("pressed", self, "_on_Button_pressed", [button.scene_to_load])
 
+func quit():
+	get_tree().quit()
+	
 func _on_Button_pressed(scene_to_load):
 	scene_path_to_load = scene_to_load
 	$FadeIn.show()

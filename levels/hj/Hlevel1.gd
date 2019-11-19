523c34dd2860d7924ff9b1c22740f8d2a8736ec5
hayoungjeon <hayoungjeon@tamu.edu>
2019-11-19 14:55:51 -0600

    [US 194] popup menu working

--- levels/hj/Hlevel1.gd
+++ levels/hj/Hlevel1.gd
@@ -1,28 +1,19 @@
 extends Node2D
-
+var gamePopUpMenu
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	gamePopUpMenu = get_node("GamePopupMenu")
+	#pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
 
 
-func _on_ResumeButton_pressed():
-	self.hide()
-	pass # Replace with function body.
-
-
-func _on_MainMenuButton_pressed():
-	get_tree().change_scene("res://title_scene/title_screen.tscn")
-	pass # Replace with function body.
-
-
-func _on_ExitButton_pressed():
-	get_tree().quit()
+func _on_MenuButton_pressed():
+	gamePopUpMenu.show()
 	#pass # Replace with function body.

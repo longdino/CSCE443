f869362f4093483f51f218ce75723872661c00e5
BradleyKost <bradleykost@tamu.edu>
2019-11-22 16:42:31 -0600

    Revert "Merge pull request #18 from arangill/BradleyKost"
    This reverts commit 8da2558198ce3c6e33807cd834e83df160b0e8b4, reversing
    changes made to cc9b92c1a40644e8bc913bef2c1a3adec7516d1d.

--- sprites/Tiles/flame.gd
+++ sprites/Tiles/flame.gd
@@ -1,4 +1,4 @@
-extends Control
+extends AnimatedSprite
 
 # Declare member variables here. Examples:
 # var a = 2
@@ -11,8 +11,3 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
-
-
-func _on_Button_pressed():
-	get_tree().change_scene('res://title_scene/title_screen.tscn')
-	pass # Replace with function body.

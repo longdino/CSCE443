ef11cfbd0bd50b841fcf2aa075096ad31c9a115d
hayoungjeon <hayoungjeon@tamu.edu>
2019-12-03 01:00:27 -0600

    more sounds working

--- levels/tutorials.gd
+++ levels/tutorials.gd
@@ -6,6 +6,7 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	MusicController.play("res://Audio/By_The_Winds.ogg")
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
